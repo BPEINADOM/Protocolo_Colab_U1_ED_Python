7ca9b3f61dd57123be95c10075d0792f37c34d5e
BPEINADOM <bpeinadom@unicartagena.edu.co>
2026-09-17 16:00:25 -0500

    feat(matrices3): sumar elementos e intercambiar filas

--- arreglosmatricespython/matrices/EjercicioMatrices.py
+++ arreglosmatricespython/matrices/EjercicioMatrices.py
@@ -29,3 +29,27 @@ for j in range(len(matriz[0])):
     for i in range(len(matriz)):
         print(matriz[i][j], end=" ")
     print()
+
+# Sumamos todos los elementos de la matriz
+suma = 0
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        suma = suma + matriz[i][j]
+
+print("\nSuma de todos los elementos:", suma)
+
+
+# Intercambiamos la primera fila con la última fila
+auxiliar = matriz[0]
+matriz[0] = matriz[-1]
+matriz[-1] = auxiliar
+
+
+# Mostramos la matriz después del intercambio
+print("\nMatriz después de intercambiar la primera y la última fila:")
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j], end=" ")
+    print()
